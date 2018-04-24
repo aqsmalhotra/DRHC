dc991a5bdefe0430e2f7ad2aae47bc8ade4767a9
Geri Kona <gerikona@hotmail.com>
2018-04-24 13:54:22 -0400

    Model/Controller Update
    Job Postings, Applications, Events

---
+++ FAQCheckpoint2/Controllers/ApplicationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FAQCheckpoint2.Models;
+
+namespace FAQCheckpoint2.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private aqsmalhotraEntities db = new aqsmalhotraEntities();
+
+        // GET: Applications
+        public ActionResult Index()
+        {
+            var applications = db.Applications.Include(a => a.Job_Postings);
+            return View(applications.ToList());
+        }
+
+        // GET: Applications/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Create
+        public ActionResult Create()
+        {
+            ViewBag.job_posting = new SelectList(db.Job_Postings, "job_id", "job_title");
+            return View();
+        }
+
+        // POST: Applications/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "app_id,app_first_name,app_last_name,app_address,app_city,app_province,app_postal_code,app_phone,app_email,app_comments,app_submission_date,job_posting")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Applications.Add(application);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.job_posting = new SelectList(db.Job_Postings, "job_id", "job_title", application.job_posting);
+            return View(application);
+        }
+
+        // GET: Applications/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.job_posting = new SelectList(db.Job_Postings, "job_id", "job_title", application.job_posting);
+            return View(application);
+        }
+
+        // POST: Applications/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "app_id,app_first_name,app_last_name,app_address,app_city,app_province,app_postal_code,app_phone,app_email,app_comments,app_submission_date,job_posting")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(application).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.job_posting = new SelectList(db.Job_Postings, "job_id", "job_title", application.job_posting);
+            return View(application);
+        }
+
+        // GET: Applications/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Application application = db.Applications.Find(id);
+            if (application == null)
+            {
+                return HttpNotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Application application = db.Applications.Find(id);
+            db.Applications.Remove(application);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FAQCheckpoint2/Controllers/Job_PostingsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FAQCheckpoint2.Models;
+
+namespace FAQCheckpoint2.Controllers
+{
+    public class Job_PostingsController : Controller
+    {
+        private aqsmalhotraEntities db = new aqsmalhotraEntities();
+
+        // GET: Job_Postings
+        public ActionResult Index()
+        {
+            var job_Postings = db.Job_Postings.Include(j => j.Department);
+            return View(job_Postings.ToList());
+        }
+
+        // GET: Job_Postings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job_Postings job_Postings = db.Job_Postings.Find(id);
+            if (job_Postings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job_Postings);
+        }
+
+        // GET: Job_Postings/Create
+        public ActionResult Create()
+        {
+            ViewBag.dept = new SelectList(db.Departments, "Id", "Name");
+            return View();
+        }
+
+        // POST: Job_Postings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "job_id,job_title,job_description,job_type,job_openings,job_posted_date,job_closing_date,dept")] Job_Postings job_Postings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Job_Postings.Add(job_Postings);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.dept = new SelectList(db.Departments, "Id", "Name", job_Postings.dept);
+            return View(job_Postings);
+        }
+
+        // GET: Job_Postings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job_Postings job_Postings = db.Job_Postings.Find(id);
+            if (job_Postings == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.dept = new SelectList(db.Departments, "Id", "Name", job_Postings.dept);
+            return View(job_Postings);
+        }
+
+        // POST: Job_Postings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "job_id,job_title,job_description,job_type,job_openings,job_posted_date,job_closing_date,dept")] Job_Postings job_Postings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job_Postings).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.dept = new SelectList(db.Departments, "Id", "Name", job_Postings.dept);
+            return View(job_Postings);
+        }
+
+        // GET: Job_Postings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job_Postings job_Postings = db.Job_Postings.Find(id);
+            if (job_Postings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job_Postings);
+        }
+
+        // POST: Job_Postings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Job_Postings job_Postings = db.Job_Postings.Find(id);
+            db.Job_Postings.Remove(job_Postings);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
