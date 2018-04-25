dbf99ebb1a7b65dac666101a0e92ad8e537871dd
Sophia <sophiavong@live.ca>
2018-04-25 18:45:38 -0400

    home/styles/images

--- FAQCheckpoint2/App_Start/RouteConfig.cs
+++ FAQCheckpoint2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FAQCheckpoint2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "FAQs", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
